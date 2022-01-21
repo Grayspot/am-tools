c642f3b30e501af36a7a0f40b33922f671a66a4e
Le Russe <grayspot@inbox.ru>
2022-01-21 15:14:28 +0100

    monitor update
    Commands now work somewhat correctly. Except stopmotion. To fix.

--- draft.py
+++ draft.py
@@ -1,4 +1,5 @@
 import serial.tools.list_ports
+import time
 import os
 
 def find_port(serial_number):
@@ -10,17 +11,35 @@ def find_port(serial_number):
 connect = find_port(serial_number='95530343434351A012C1')
 print(connect)
 
-ser = serial.Serial(connect.port, baudrate=115200)
+ser = serial.Serial(connect.port, baudrate=115200, bytesize=serial.EIGHTBITS, timeout=1)
 if(ser.isOpen() == False):
     ser.open()
+time.sleep(1)
 
 cmd=""
 
+############
+# CMD LIST #
+############
+# INITAXISORIGINS - OK
+# GOTOPOSITION,X,Y - OK
+# STOPMOTION - KO - zero reaction
+# SETSPEED - OK
+# SETSTROKE - ???
+# BRKENGAGE - OK
+# BRKRELEASE - OK
+#
+
 while(cmd!="exit"):
     os.system('cls' if os.name == 'nt' else 'clear')
+    ser.flushInput()
     #INITAXISORIGINS
+    #GOTOPOSITION
+
     cmd=input(">> ")
     if(cmd!="exit"):
+        cmd=cmd+os.linesep
+        ser.reset_input_buffer()
         ser.write(cmd.encode())
     arduinoData = ser.readline().decode('ascii')
     print(arduinoData)
